4ca05db0cb4a0d199bb14bb0f7c1c9e5c3e61fa9
Bas van der Hoek <bas@hoek.com>
2019-01-23 13:47:38 +0100

    find path from symlink

--- mnemonic.sh
+++ mnemonic.sh
@@ -1,1 +1,8 @@
-grep "$1" "`dirname $0`/OpenTaal-210G-basis-gekeurd.txt" | awk '{ print length, $0 }' | sort | cut -d" " -f2-
+if readlink $0 >/dev/null 2>&1; then
+	DIR="$(dirname $(readlink $0))"
+else
+	DIR="$(dirname $0)"
+fi
+echo $DIR
+
+grep "$1" "$DIR/OpenTaal-210G-basis-gekeurd.txt" | awk '{ print length, $0 }' | sort | cut -d" " -f2-
